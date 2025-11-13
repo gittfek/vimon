28647de884b26d7b755175baa81ef3046b2bc688
gittfek <github@gewebb.se>
2025-11-13 15:25:14 +0100

    fix: update Stripe API version to 2025-08-27

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
+  /*experimental: {
     ppr: true,
     clientSegmentCache: true,
     nodeMiddleware: true
-  }
+  }*/
 };
 
 export default nextConfig;
