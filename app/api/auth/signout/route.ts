e3c2350935e477de800756a084e30e180b8cbf7d
gittfek <github@gewebb.se>
2025-11-16 22:55:31 +0100

    Update route.ts

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const cookieStore = await cookies();
-  cookieStore.delete('session', { path: '/' });
+  cookieStore.delete('session');
 
   return NextResponse.redirect(new URL('/', request.url));
 }
